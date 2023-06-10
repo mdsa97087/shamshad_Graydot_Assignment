dfedf664e09c463a39aa6bf864ae34282ffb4da2
mdsa97087 <mdsa97087@gmail.com>
2023-06-10 21:58:58 +0530

    add

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const dropContainer = document.getElementById("dropContainer");
+const dragContainer = document.getElementById("dragContainer");
 const dragItems = document.querySelectorAll(".imageDiv");
 
 // Add event listeners to each drag item
@@ -12,8 +13,7 @@ dropContainer.addEventListener("dragenter", dragEnter);
 dropContainer.addEventListener("dragleave", dragLeave);
 dropContainer.addEventListener("drop", drop);
 
-let draggedItem = null;
-
+let draggedItem ;
 function dragStart(event) {
   draggedItem = this;
   this.classList.add("dragging");
@@ -33,64 +33,14 @@ function dragLeave() {
 }
 
 function drop() {
-  // dropContainer.classList.remove('active');
   this.append(draggedItem);
   draggedItem.classList.remove("dragging");
   alert("Item dropped!");
-  // draggedItem = null;
 }
 
-// function showMessage(message) {
-//   const messageContainer = document.createElement('div');
-//   messageContainer.textContent = message;
-//   dropContainer.appendChild(messageContainer);
-// }
 
 function reset() {
   dropContainer.innerHTML = "";
-  // dropContainer.innerHTML = 'Drop items here';
-  // showMessage('');
-}
-
-//     Reverse Item From Second Div
-
-const dragContainer = document.getElementById("dragContainer");
-const dragItems2 = document.querySelectorAll(".imageDiv");
-
-// Add event listeners to each drag item
-dragItems2.forEach((item) => {
-  item.addEventListener("dragstart", dragStart2);
-});
-
-// Add event listeners to the drop container
-dragContainer.addEventListener("dragover", dragOver2);
-dragContainer.addEventListener("dragenter", dragEnter2);
-dragContainer.addEventListener("dragleave", dragLeave2);
-dragContainer.addEventListener("drop", drop2);
-
-let draggedItem2 = null;
-
-function dragStart2(event) {
-  draggedItem2 = this;
-  this.classList.add("dragging");
-}
-
-function dragOver2(event) {
-  event.preventDefault();
-}
-
-function dragEnter2(event) {
-  event.preventDefault();
-  dragContainer.classList.add("active");
-}
-
-function dragLeave2() {
-  dragContainer.classList.remove("active");
-}
-
-function drop2() {
-  dragContainer.classList.remove("active");
-  this.append(draggedItem2);
-  draggedItem2.classList.remove("dragging");
+  dragContainer.append(draggedItem);  
 }
 
